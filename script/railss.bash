cd437646c6c51805b72d5f5d8f86b1837e6b92fe
Dan Bikle <bikle101@outlook.com>
2018-12-21 20:52:24 -0800

    more

---
+++ script/railss.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# railss.bash
+
+# This script should start a webserver for development efforts.
+
+cd ${HOME}/reg4/ # assuming I put the repo in ${HOME}/reg4/
+
+# I should start a rails server on all interfaces on port 4742:
+bin/rails server -b 0.0.0.0 -p 4742
+
+exit
